a4354f02c8c0fda5c3c297298fed00185497f614
fallow5 <fallowu@gmail.com>
2013-08-28 21:34:21 +0800

    first commit

---
+++ common.mk
@@ -0,0 +1,48 @@
+#Common headers
+common_includes := hardware/qcom/display/libgralloc
+common_includes += hardware/qcom/display/liboverlay
+common_includes += hardware/qcom/display/libcopybit
+common_includes += hardware/qcom/display/libqdutils
+common_includes += hardware/qcom/display/libhwcomposer
+common_includes += hardware/qcom/display/libexternal
+common_includes += hardware/qcom/display/libqservice
+
+common_header_export_path := qcom/display
+
+#Common libraries external to display-caf HAL
+common_libs := liblog libutils libcutils libhardware
+
+#Common C flags
+common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers
+
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+    common_flags += -D__ARM_HAVE_NEON
+endif
+
+ifneq ($(filter msm8974 msm8x74 msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
+    # TODO: This define makes us pick a few inline functions
+    # from the kernel header media/msm_media_info.h. However,
+    # the bionic clean_headers utility scrubs them out.
+    # Figure out a way to import those macros correctly
+    # common_flags += -DVENUS_COLOR_FORMAT
+    common_flags += -DMDSS_TARGET
+endif
+
+# Executed only on QCOM BSPs
+ifeq ($(TARGET_USES_QCOM_BSP),true)
+# This flag is used to compile out any features that depend on framework changes
+    common_flags += -DQCOM_BSP
+endif
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+    common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+    kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+endif
+
+ifeq ($(TARGET_DISPLAY_USE_RETIRE_FENCE),true)
+    common_flags += -DUSE_RETIRE_FENCE
+endif
+
+ifneq ($(TARGET_DISPLAY_INSECURE_MM_HEAP),true)
+    common_flags += -DSECURE_MM_HEAP
+endif
